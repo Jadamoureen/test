3417757eee4407e48f7f3e3e283fbb88e8622144
jadamoureen <moureen.caroline@gmail.com>
2022-07-22 05:55:40 +0300

    Uploading test files

---
+++ test.py
@@ -0,0 +1,9 @@
+#List comphrension
+
+#finidng the even numbers
+
+nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+even_nums = [i for i in nums if i%2 == 0]
+
+print(even_nums)
---
+++ test1.py
@@ -0,0 +1,9 @@
+#List comphrension
+
+#finidng the odd numbers
+
+nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+odd_nums = [i for i in nums if i%2==1]
+
+print(odd_nums)
